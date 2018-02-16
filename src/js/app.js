ceb5b564f5251dcd7e44c1410945e873f68d78bd
Damian Soria <damian.soria.web@gmail.com>
2018-02-16 18:44:21 -0300

    Fixed

--- src/js/app.js
+++ src/js/app.js
@@ -4,9 +4,9 @@ app.controller('mainController', function($scope, $http){
         method: 'GET',
         url: 'src/js/news.json'
      }).then(function (response){
-        //$scope.news = response;
+        $scope.news = response.rss.channel;
         //Imprimo en consola el response
-        console.log(response);
+        console.log(response.rss.channel);
      },function (error){
         console.log('Error: ' + error);
      });
