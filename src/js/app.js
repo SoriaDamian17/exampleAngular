cd76445cee244a208f3bdb14d53038ac2171ef2f
Damian Soria <damian.soria.web@gmail.com>
2018-02-16 18:35:09 -0300

    Fixed

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,14 @@
 var app = angular.module('app', []);
 app.controller('mainController', function($scope, $http){
     var news = null;//Creamos variable para nuestras noticias
-    $http.get('src/js/news.json')
-    .success(function(response) {
-        //news = response;
+    $http({
+        method: 'GET',
+        url: 'src/js/news.json'
+     }).then(function (response){
         $scope.news = response;
         //Imprimo en consola el response
         console.log(response);
-    })
-    .error(function(err) {
-        console.log('Error: ' + err);
-    });
+     },function (error){
+        console.log('Error: ' + error);
+     });
 });
