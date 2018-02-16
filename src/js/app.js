e4bc91f68483defe13abe6d77a62ab52da88f407
Damian Soria <damian.soria.web@gmail.com>
2018-02-16 18:41:50 -0300

    fixed json error

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,10 @@
 var app = angular.module('app', []);
 app.controller('mainController', function($scope, $http){
-    var news = null;//Creamos variable para nuestras noticias
     $http({
         method: 'GET',
         url: 'src/js/news.json'
      }).then(function (response){
-        $scope.news = response;
+        //$scope.news = response;
         //Imprimo en consola el response
         console.log(response);
      },function (error){
