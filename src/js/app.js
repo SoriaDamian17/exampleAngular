8031613c33b42628480d98c2a806369b206390b5
Damian Soria <damian.soria.web@gmail.com>
2018-02-16 18:32:37 -0300

    Fixed

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,8 @@ app.controller('mainController', function($scope, $http){
     var news = null;//Creamos variable para nuestras noticias
     $http.get('src/js/news.json')
     .success(function(response) {
-        news = response;
-        //$scope.news = response;
+        //news = response;
+        $scope.news = response;
         //Imprimo en consola el response
         console.log(response);
     })
